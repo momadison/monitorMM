0f03011db45bd8cfa36ae7ae0f104b27b32e6f6f
Matthew-Madison <matthew.madison@ibm.com>
2020-02-03 21:39:46 -0600

    u

--- register_my_custom_function.py
+++ register_my_custom_function.py
@@ -14,6 +14,6 @@ db_schema = None
 db = Database(credentials=credentials)
 
 
-from customMOM.functions import multiplyByFactorMM
+from customMOM.functions import conditionCountBool
 
-db.register_functions([multiplyByFactorMM])
+db.register_functions([conditionCountBool])
