b444766822b6b73bf03f7e8287dc7f9a2f6c8ba4
Matthew-Madison <matthew.madison@ibm.com>
2020-02-18 12:36:01 -0600

    u

--- local_test_of_function.py
+++ local_test_of_function.py
@@ -93,7 +93,7 @@ print(df)
 
 '''
 
-from customMOM.functions import firstOccurenceRelation
+from customMOM.functions import multiplyByFactorMM
 
 d = {'id': ['TestdeviceWhiOffline','TestdeviceWhiOffline','TestdeviceWhiBatterycritical','TestdeviceWhiBatteryLow','TestdeviceWhiNormal','TestdeviceWhiWaterleak','TestdeviceWhiNormal','TestdeviceWhiWaterleak'],
      'RCV_TIMESTAMP_UTC': [pd.to_datetime('2020-01-21 10:50:36.604000'),pd.to_datetime('2020-01-21 10:50:44.524000'),pd.to_datetime('2020-01-26 09:53:04.067000'),pd.to_datetime(' 2020-01-27 09:53:10.130000'),pd.to_datetime(' 2020-01-27 09:53:10.130000'),pd.to_datetime(' 2020-01-27 09:53:10.130000'),pd.to_datetime(' 2020-01-27 09:53:10.130000'),pd.to_datetime('2020-01-28 09:53:10.130000')],
@@ -112,6 +112,7 @@ d = {'id': ['TestdeviceWhiOffline','TestdeviceWhiOffline','TestdeviceWhiBatteryc
      'hazard2': [6,'NaN','NaN','NaN','NaN','NaN','NaN','NaN'],
      'hazard3': [4,'NaN','NaN','NaN','NaN','NaN','NaN','NaN'],
      'waterAlert2': [1.000,0.000,None,1.000,0.000,None,1.000,0.000],
+     'waterAlertCount': [1,1,2,2,3,3,3,3],
      'deploymentCount': [12,12,12,12,12,12,12,12],
      'isOnline': [True,True,True,False,False,True,True,True],
      'deploymentDrop': ['testpolicybatterycritical','testpolicywaterleak','testpolicyoffline','testpolicynormal','whitestpolicybatterylow',None,None,None],
@@ -120,11 +121,12 @@ d = {'id': ['TestdeviceWhiOffline','TestdeviceWhiOffline','TestdeviceWhiBatteryc
      }
 df = pd.DataFrame(data=d)
 
-fn = firstOccurenceRelation(
-     input_items=['manufacturerDeviceId'],
-     input_items2=['state'],
+fn = multiplyByFactorMM(
+     input_items=['waterAlertCount'],
+     #input_items2=['state'],
      output_items=['new_column'],
-     condition='Texas'
+     #condition='Texas',
+     factor = 2225
 )
 
 #df = fn.execute_local_test(db=db, db_schema=db_schema, generate_day=1, to_csv=True)
